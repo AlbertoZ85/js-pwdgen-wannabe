71e4b55c4a5f98ed313200a2abc44c6033033a19
Alberto Zordan <zorberto@gmail.com>
2020-07-06 16:40:29 +0200

    .toLowerCasa method tried. Succeed!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+// var nome = prompt('Inserisci il tuo nome');
+// var cognome = prompt('Inserisci il tuo cognome');
+// var colorePreferito = prompt('Qual è il tuo colore preferito?');
+//
+// document.getElementById('presentazione').innerHTML= nome + cognome + colorePreferito + 19;
+
 var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 var colorePreferito = prompt('Qual è il tuo colore preferito?');
+var res = (nome + cognome + colorePreferito).toLowerCase();
 
-document.getElementById('presentazione').innerHTML= nome + cognome + colorePreferito + 19;
+document.getElementById('presentazione').innerHTML= res + 19;
