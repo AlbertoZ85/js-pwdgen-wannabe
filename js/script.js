3dd8c4b09c5cd9e36045e70f576bf103deae186e
Alberto Zordan <zorberto@gmail.com>
2020-07-06 16:26:22 +0200

    Done!

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colorePreferito = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('presentazione').innerHTML= nome + cognome + colorePreferito + 19;
